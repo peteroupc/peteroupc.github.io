959416be449eb8771345e2f3ab778209af3cef17
Peter O <poccil14@gmail.com>
2015-04-25 10:34:35 -0400

    Updating GitHub Pages

--- canvasback/canvasback.js
+++ canvasback/canvasback.js
@@ -7,42 +7,6 @@ If you like this, you should donate to Peter O.
 at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
 */
 (function(window){
-var FrameCounter=function(){
- this.fps=-1;
- this.lastFrame=-1;
- this.frameGaps=[]
- this.frameCount=0;
-}
-FrameCounter.prototype.update=function(){
- var now=("performance" in window) ?
-   window.performance.now() : (new Date().getTime()*1000);
-  if(this.lastFrame>=0){
-    var gap=now-this.lastFrame;
-    if(this.frameGaps.length>300)
-     this.frameGaps.shift();
-    if(gap>5000){
-     // treat as a discontinuity, so discard all the
-     // frame gaps recorded so far
-     this.frameGaps=[];
-    }
-    this.frameGaps.push(gap);
-  }
-  this.lastFrame=now;
-  this.frameCount++;
-  if(this.frameGaps.length>0 && this.frameCount>=30){
-    this.frameCount=0;
-    var total=0;
-    for(var i=0;i<this.frameGaps.length;i++){
-      total+=this.frameGaps[i];
-    }
-    total/=1.0*this.frameGaps.length;
-    this.fps=(total<=0) ? 60 : 1000.0/total;
-  }
-}
-FrameCounter.prototype.getFPS=function(){
- return this.fps;
-}
-
 var CanvasBackground=function(color){
   "use strict";
   color=color||"#ff0000";
