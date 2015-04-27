2d00ed9215bdc50c182573ade1ac00e6cffd3cc3
Peter O <poccil14@gmail.com>
2015-04-27 04:57:28 -0400

    Updating GitHub Pages

--- html3dutil/camera.js
+++ html3dutil/camera.js
@@ -16,13 +16,13 @@
 * to be seen.
 */
 function Camera(scene, fov, nearZ, farZ){
- this.target=[0,0,0]; // target position
  this.distance=Math.max(nearZ,10); // distance from the target in units
  this.position=[0,0,this.distance];
- this.angleQuat=GLMath.quatIdentity();
+ this.inverseQuat=GLMath.quatIdentity();
+ this.scene=scene;
  this.angleX=0;
  this.angleY=0;
- this.scene=scene;
+ this.oldpos=[null,null];
  this.fov=fov;
  this.near=nearZ;
  this.far=farZ;
@@ -31,256 +31,74 @@ function Camera(scene, fov, nearZ, farZ){
  }
  this.currentAspect=this.scene.getAspect();
  this.scene.setPerspective(this.fov,this.currentAspect,this.near,this.far);
- this._updateLookAt();
-}
-Camera._vec3diff=function(a,b){
- return [a[0]-b[0],a[1]-b[1],a[2]-b[2]];
-}
-/**
-* Moves the camera to the left or right, adjusting
-* its angle, while maintaining its distance to the target
-* position.
-* @param {number} angleDegrees Angle, in degrees,
-* to move the camera.  Positive angles mean left,
-* negative angles mean right.
-* @return {Camera} This object.
-*/
-Camera.prototype.moveAngleHorizontal=function(angleDegrees){
- if(angleDegrees!=0){
-  this._angleHorizontal(-angleDegrees);
-  this._resetPosition();
- }
- return this;
+ this._updateView();
 }
 /** @private */
-Camera.prototype._angleHorizontal=function(angleDegrees){
- var change=((angleDegrees>=0 && angleDegrees<360) ? angleDegrees : ((angleDegrees%360)+(angleDegrees<0 ? 360 : 0)))*GLMath.PiDividedBy180;
- this.angleY+=change;
- this.angleY=((this.angleY>=0 && this.angleY<GLMath.PiTimes2) ? this.angleY : ((this.angleY%GLMath.PiTimes2)+(this.angleY<0 ? GLMath.PiTimes2 : 0)))
- this.angleQuat=GLMath.quatMultiply(this.angleQuat,
-   GLMath.quatFromAxisAngle(change*GLMath.Num180DividedByPi,0,1,0));
- GLMath.quatNormInPlace(this.angleQuat);
-}
-/** @private */
-Camera.prototype._angleVertical=function(angleDegrees){
- var change=((angleDegrees>=0 && angleDegrees<360) ? angleDegrees : ((angleDegrees%360)+(angleDegrees<0 ? 360 : 0)))*GLMath.PiDividedBy180;
- this.angleX+=change;
- this.angleX=((this.angleX>=0 && this.angleX<GLMath.PiTimes2) ? this.angleX : ((this.angleX%GLMath.PiTimes2)+(this.angleX<0 ? GLMath.PiTimes2 : 0)))
- this.angleQuat=GLMath.quatMultiply(this.angleQuat,
-   GLMath.quatFromAxisAngle(change*GLMath.Num180DividedByPi,1,0,0));
- GLMath.quatNormInPlace(this.angleQuat);
-}
-
-/**
-* Moves the camera up or down, adjusting its angle, while
-* maintaining its distance to the target position.
-* @param {number} angleDegrees Angle, in degrees,
-* to move the camera.  Positive angles mean down,
-* negative angles mean up.
-* @return {Camera} This object.
-*/
-Camera.prototype.moveAngleVertical=function(angleDegrees){
- if(angleDegrees!=0){
-  this._angleVertical(-angleDegrees);
-  this._resetPosition();
- }
+Camera.prototype._updateView=function(){
+ var look=GLMath.mat4identity();
+ look=GLMath.mat4multiply(look,
+   GLMath.quatToMat4(this.inverseQuat));
+ look=GLMath.mat4translate(look,
+   this.position[0],this.position[1],this.position[2]);
+ this.scene.setViewMatrix(GLMath.mat4invert(look));
  return this;
 }
-
-/**
-* Turns the camera up or down, maintaining
-* its current position.
-* @param {number} angleDegrees Angle, in degrees,
-* to move the camera.  Positive angles mean down,
-* negative angles mean up.
-* @return {Camera} This object.
-*/
-Camera.prototype.turnVertical=function(angleDegrees){
- if(angleDegrees!=0){
-  var pos=this.getPosition();
-  this._angleVertical(angleDegrees);
-  this._resetTarget();
- }
- return this;
-}
-/** @private */
-Camera.prototype._resetPosition=function(){
-  var diff=Camera._vec3diff(this.target,this.position);
-  var dist=GLMath.vec3length(diff);
-  var newVector=GLMath.quatTransform(this.angleQuat,[0,0,-dist]);
-  this.position[0]=this.target[0]-newVector[0];
-  this.position[1]=this.target[1]-newVector[1];
-  this.position[2]=this.target[2]-newVector[2];
-  this._updateLookAt();
-  this.setDistance(this.distance);
-}
-/** @private */
-Camera.prototype._resetTarget=function(){
-  var diff=Camera._vec3diff(this.target,this.position);
-  var dist=GLMath.vec3length(diff);
-  var newVector=GLMath.quatTransform(this.angleQuat,[0,0,-dist]);
-  this.target[0]=this.position[0]+newVector[0];
-  this.target[1]=this.position[1]+newVector[1];
-  this.target[2]=this.position[2]+newVector[2];
-  this._updateLookAt();
-  this.setDistance(this.distance);
-}
-
-/**
-* Turns the camera to the left or right, maintaining
-* its current position.
-* @param {number} angleDegrees Angle, in degrees,
-* to move the camera.  Positive angles mean right,
-* negative angles mean left.
-* @return {Camera} This object.
-*/
-Camera.prototype.turnHorizontal=function(angleDegrees){
- if(angleDegrees!=0){
-  var pos=this.getPosition();
-  this._angleHorizontal(-angleDegrees);
-  this._resetTarget();
- }
- return this;
-}
-
-/**
-* Gets the current position of the camera.
-* @return {Camera} A 3-element array giving
-* the X, Y, and Z coordinates, respectively, of
-* the camera's position.
-*/
-Camera.prototype.getPosition=function(){
-  return this.position.slice(0,3);
-}
-/**
- * Not documented yet.
- * @param {*} dist
- */
 Camera.prototype.setDistance=function(dist){
- if(dist<0)throw new Error("invalid distance")
- if(dist<this.near)dist=this.near;
- var diff=Camera._vec3diff(this.target,this.position);
- var curdist=GLMath.vec3length(diff);
- var distdiff=curdist-diff;
- this.distance=dist;
- if(distdiff!=0){
-  var factor=dist/curdist;
-  this.position[0]=this.target[0]-diff[0]*factor;
-  this.position[1]=this.target[1]-diff[1]*factor;
-  this.position[2]=this.target[2]-diff[2]*factor;
-  this._updateLookAt();
- }
- return this;
+ this.distance=Math.max(this.near,dist);
+ this.position=[0,0,this.distance]
+ return this._updateView();
 }
 
-/**
-* Moves the camera's position to the given
-* X, Y, and Z coordinates, and adjusts the target's
-* position to maintain the current distance and
-* orientation.
-* @param {number} X coordinate.
-* @param {number} Y coordinate.
-* @param {number} Z coordinate.
-* @return {Camera} This object.
-*/
-Camera.prototype.movePosition=function(cx, cy, cz){
- if(cx!=0 && cy!=0 && cz!=0){
-  var pos=this.getPosition();
-  this.target[0]+=(cx-pos[0]);
-  this.target[1]+=(cy-pos[1]);
-  this.target[2]+=(cz-pos[2]);
-  this.position=[cx,cy,cz];
- }
- return this;
+Camera.prototype._orbit=function(deltaMouseX,deltaMouseY,angleMultiplier){
+  var x=deltaMouseX
+  var y=deltaMouseY
+  this.angleX+=x*angleMultiplier;
+  this.angleY+=y*angleMultiplier;
+  this.angleX=((this.angleX>=0 && this.angleX<360) ? this.angleX :
+     ((this.angleX%360)+(this.angleX<0 ? 360 : 0)));
+  if(this.angleY>=89.99999)this.angleY=89.99999;
+  if(this.angleY<=-89.99999)this.angleY=-89.99999;
+  this.inverseQuat=GLMath.quatFromTaitBryan(
+   this.angleY,this.angleX,0,GLMath.RollYawPitch);
+  this.inverseQuat=GLMath.quatInverse(this.inverseQuat);
 }
 
-/**
-* Moves the camera closer or farther to the target
-* position.  The camera won't move closer than the distance
-* to the near clipping plane.
-* @param {number} distance Distance, in world
-* space units, to move the camera.  Positive values
-* mean closer, negative angles mean farther.
-* @return {Camera} This object.
-*/
-Camera.prototype.moveClose=function(dist){
- if(dist!=0){
-  var diff=Camera._vec3diff(this.target,this.position);
-  var realDist=GLMath.vec3length(diff);
-  var newDist=realDist-dist;
-  if(newDist<this.near){
-   newDist=this.near; // too close
+Camera.prototype._trackball=function(deltaMouseX,deltaMouseY,angleMultiplier){
+  var x=deltaMouseX
+  var y=deltaMouseY
+  var n=x*x+y*y;
+  if(n>0.0){
+   n=Math.sqrt(n);
+   var a=GLMath.vec3scaleInPlace([y,x,0],1.0/n);
+   var rot=GLMath.quatFromAxisAngle(
+     -n*angleMultiplier,a[0],a[1],a[2]);
+   this.inverseQuat=GLMath.quatMultiply(this.inverseQuat,rot);
+   GLMath.quatNormInPlace(this.inverseQuat);
   }
-  this.distance=newDist;
-  var factor=newDist/realDist;
-  this.position[0]=this.target[0]-diff[0]*factor;
-  this.position[1]=this.target[1]-diff[1]*factor;
-  this.position[2]=this.target[2]-diff[2]*factor;
-  this._updateLookAt();
- }
- return this;
 }
 
-/**
-* Moves the target and the camera either forward or back,
-* maintaining the distance between the camera and the target.
-* @param {number} distance Distance, in world
-* space units, to move the target and camera, either forward
-* or back.  Positive values
-* cause the camera to move forward, negative angles
-* cause it to move back.
-* @return {Camera} This object.
-*/
-Camera.prototype.moveForward=function(dist){
- if(dist!=0){
-  var diff=Camera._vec3diff(this.target,this.position);
-  var realDist=GLMath.vec3length(diff);
-  var factor=dist/realDist;
-  this.position[0]+=diff[0]*factor;
-  this.position[1]+=diff[1]*factor;
-  this.position[2]+=diff[2]*factor;
-  this.target[0]+=diff[0]*factor;
-  this.target[1]+=diff[1]*factor;
-  this.target[2]+=diff[2]*factor;
-  this._updateLookAt();
- }
- return this;
-}
-/** @private */
-Camera.prototype._updateLookAt=function(){
- this.scene.setLookAt(this.getPosition(),this.target,
-   GLMath.quatTransform(this.angleQuat,[0,1,0]));
-}
-/**
- * Not documented yet.
-* @return {Camera} This object.
- */
 Camera.prototype.update=function(){
  var delta=this.input.deltaXY();
  if(this.input.leftButton){
-  this.moveAngleHorizontal(0.5*delta.x)
-  this.moveAngleVertical(0.5*delta.y)
+  this._trackball(delta.x,delta.y,0.3);
+ } else {
+  this.oldpos=[delta.cx,delta.cy]
  }
  if(this.input.keys[InputTracker.DOWN]){
-  this.moveForward(-0.2)
+  this.setDistance(this.distance+0.2)
  }
  if(this.input.keys[InputTracker.UP]){
-  this.moveForward(0.2)
- }
- if(this.input.keys[InputTracker.LEFT]){
-  this.turnHorizontal(-0.5)
- }
- if(this.input.keys[InputTracker.RIGHT]){
-  this.turnHorizontal(0.5)
+  this.setDistance(this.distance-0.2)
  }
  var aspect=this.scene.getAspect();
  if(aspect!=this.currentAspect){
   this.currentAspect=aspect;
   this.scene.setPerspective(this.fov,this.currentAspect,this.near,this.far);
  }
- this._updateLookAt();
- return this;
+ return this._updateView();
 }
+///////////////////////////////////////////////////////////////
+
 function InputTracker(element){
  this.leftButton=false;
  this.rightButton=false;
@@ -325,6 +143,7 @@ function InputTracker(element){
   thisObj.clientY=e.clientY-InputTracker._getPageY(e.target);
  })
 };
+
 /** @private */
 InputTracker._getPageX=function(o) {
  "use strict";
@@ -374,7 +193,7 @@ InputTracker.prototype.deltaXY=function(){
  var deltaX=0;
  var deltaY=0;
  if(this.clientX==null || this.clientY==null){
-  return {"x":0,"y":0};
+  return {"x":0,"y":0,"cx":0,"cy":0};
  }
  var deltaX=(this.lastClient.length==0) ? 0 :
    this.clientX-this.lastClient[0];
@@ -382,5 +201,6 @@ InputTracker.prototype.deltaXY=function(){
    this.clientY-this.lastClient[1];
  this.lastClient[0]=this.clientX;
  this.lastClient[1]=this.clientY;
- return {"x":deltaX,"y":deltaY};
+ return {"x":deltaX,"y":deltaY,
+   "cx":this.clientX,"cy":this.clientY};
 }
