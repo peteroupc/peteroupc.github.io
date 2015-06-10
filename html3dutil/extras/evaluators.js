f13a9a0599e79b8996f974efb4d22c5984410e60
Peter O <poccil14@gmail.com>
2015-06-10 00:40:40 -0400

    Updating GitHub Pages

--- html3dutil/extras/evaluators.js
+++ html3dutil/extras/evaluators.js
@@ -18,7 +18,8 @@ at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
  * &lt;script type="text/javascript" src="extras/evaluators.js">&lt;/script></pre>
 * @class
 * @alias SurfaceOfRevolution
-* @param {Function} curve Curve to rotate about the Z-axis.
+* @param {Function} curve Curve to rotate about the axis of rotation, as
+* specified in the "axis" parameter.
 * The curve function must contain a function
 * named "evaluate", which takes the following parameter:<ul>
 * <li><code>u</code> - A curve coordinate, generally from 0 to 1.
@@ -26,17 +27,28 @@ at: http://upokecenter.dreamhosters.com/articles/donate-now-2/
 * The evaluator function returns an array of at least 2 elements: the first
 * element is the X coordinate of the curve's position, and the second
 * element is the Y coordinate.<p>
-* If the curve function draws a curve that goes around the Z-axis, such
+* If the curve function draws a curve that goes around the axis of rotation, such
 * as a circle or ellipse, the V-coordinates given in _minval_ and _maxval_ must
-* restrict the curve definition to half of the curve.
+* restrict the curve definition to no more than half of the curve.
 * @param {number} minval Smallest V-coordinate.
 * @param {number} maxval Largest V-coordinate.   If _minval_ is greater than
 * _maxval_, both values will be swapped.
+* @param {Array<number>|undefined} axis Axis of rotation, around which the curve
+* will be rotated to generate the surface of revolution.  If null or omitted, the positive
+* Z-axis will be the axis of rotation.  This parameter is a 3-element array describing
+* the X, Y, and Z coordinates, respectively, of a 3D point.  The axis of rotation will
+* run in the direction from the origin to the point given in this parameter.  This
+* parameter need not be normalized to unit length.
 */
-var SurfaceOfRevolution=function(curve,minval,maxval){
+var SurfaceOfRevolution=function(curve,minval,maxval, axis){
  this.curve=curve
  this.minval=Math.min(minval,maxval);
  this.maxval=Math.min(minval,maxval);
+ this._axis=axis;
+ this._axisQuat=null;
+ if(this._axis){
+  this._axisQuat=GLMath.quatFromVectors([0,0,1],this._axis);
+ }
  this.evaluate=function(u,v){
   v=minval+(maxval-minval)*v;
   var curvepos=this.curve.evaluate(v);
@@ -48,25 +60,68 @@ var SurfaceOfRevolution=function(curve,minval,maxval){
   var x=cp1*cosu
   var y=cp1*sinu
   var z=cp0;
-  return [x,y,z];
+  var ret=[x,y,z];
+  if(this._axisQuat){
+   SurfaceOfRevolution._quatTransformInPlace(this._axisQuat,ret);
+  }
+  return ret;
  }
 }
+/** @private */
+SurfaceOfRevolution._quatTransformInPlace=function(q,v){
+ var v1 = GLMath.vec3cross( q, v );
+ v1[0] += v[0] * q[3];
+ v1[1] += v[1] * q[3];
+ v1[2] += v[2] * q[3];
+ var v2 = GLMath.vec3cross( v1, q );
+ var dot = q[0] * v[0] + q[1] * v[1] + q[2] * v[2];
+ v[0]=q[0] * dot + v1[0] * q[3] - v2[0];
+ v[1]=q[1] * dot + v1[1] * q[3] - v2[1];
+ v[2]=q[2] * dot + v1[2] * q[3] - v2[2];
+};
 /**
 * Creates a parametric evaluator for a surface of revolution
 * whose curve is the graph of a single-variable function.
 * @param {Function} func Function whose graph will be
-* rotated about the Z-axis.  The function takes a number
-* as a single parameter and returns a number.
+* rotated about the axis of rotation, as
+* specified in the "axis" parameter.  The function takes a number
+* as a single parameter and returns a number.  The return
+* value is effectively the radius of each part of the surface
+* from beginning to end.
 * @param {number} minval Smallest parameter of the function.
-* @param {number} maxval Largest parameter of the function.  If _minval_ is greater than
-* _maxval_, both values will be swapped.
+* This is a number of units from the origin along the axis of rotation.
+* @param {number} maxval Largest parameter of the function.
+* This is a number of units from the origin along the axis of rotation.
+* If _minval_ is greater than _maxval_, both values will be swapped.
+* @param {Array<number>|undefined} axis Axis of rotation, around which the
+* function graph will be rotated to generate the surface of revolution.
+* If null or omitted, the positive Z-axis will be the axis of rotation.
+* This parameter is a 3-element array describing
+* the X, Y, and Z coordinates, respectively, of a 3D point.  The axis of rotation will
+* run in the direction from the origin to the point given in this parameter.  This
+* parameter need not be normalized to unit length.
 * @return {SurfaceOfRevolution}
+* @example <caption>The following creates an evaluator for a cone
+* which starts as the origin and runs 10 units along the Z axis.</caption>
+* var surf=SurfaceOfRevolution.fromFunction(
+*  function(x){  return x/2; }, // use a constantly increasing function
+* 0, 10);
+* @example <caption>This is an evaluator for the same cone, but
+* shifted 3 units back.</caption>
+* var surf=SurfaceOfRevolution.fromFunction(
+*  function(x){  x+=3; return x/2; },
+* -3,7);
+* @example <caption>The following creates an evaluator for a cylinder
+* which runs from 5 to 10 units, and with a radius of 2 units.</caption>
+* var surf=SurfaceOfRevolution.fromFunction(
+*  function(x){  return 2; }, // use a constant radius
+* 5, 10);
 */
-SurfaceOfRevolution.fromFunction=function(func,minval,maxval){
+SurfaceOfRevolution.fromFunction=function(func,minval,maxval,axis){
   return new SurfaceOfRevolution({
     "evaluate":function(u){
       return [u,func(u),0];
-    }},minval,maxval);
+    }},minval,maxval,axis);
 }
 /**
 * Parametric evaluator for a torus, a special case of a surface of revolution.
@@ -85,13 +140,20 @@ SurfaceOfRevolution.fromFunction=function(func,minval,maxval){
 * The evaluator function returns an array of at least 2 elements: the first
 * element is the X coordinate of the curve's position, and the second
 * element is the Y coordinate.  If null or omitted, uses a circular cross section.
+* @param {Array<number>|undefined} axis Axis of rotation, around which the
+* function graph will be rotated to generate the surface of revolution.
+* If null or omitted, the positive Z-axis will be the axis of rotation.
+* This parameter is a 3-element array describing
+* the X, Y, and Z coordinates, respectively, of a 3D point.  The axis of rotation will
+* run in the direction from the origin to the point given in this parameter.  This
+* parameter need not be normalized to unit length.
 * @return {SurfaceOfRevolution}
 */
-SurfaceOfRevolution.torus=function(outerRadius,innerRadius,curve){
+SurfaceOfRevolution.torus=function(outerRadius,innerRadius,curve,axis){
   if(!curve)curve={
     "evaluate":function(u){
       u*=GLMath.PiTimes2;
-      return [Math.sin(u),Math.cos(v)]
+      return [Math.cos(u),Math.sin(u)]
     }
   }
   return new SurfaceOfRevolution({
@@ -100,7 +162,7 @@ SurfaceOfRevolution.torus=function(outerRadius,innerRadius,curve){
       var x=innerRadius*curvept[1];
       var y=outerRadius+innerRadius*curvept[0];
       return [x,y,0];
-    }},0,Math.PI);
+    }},0,Math.PI,axis);
 }
 
 /**
@@ -129,8 +191,7 @@ function Hypotrochoid(outerRadius, innerRadius, distFromInnerCenter){
  /**
 * Generates a point on the curve from the given u coordinate.
 * @function
-* @param {number} v V coordinate.  This will sweep around the extruded
-* tube.
+* @param {number} u U coordinate.
 * @return {Array<number>} A 3-element array specifying a 3D point.
 * Only the X and Y coordinates will be other than 0.
 */
@@ -153,7 +214,7 @@ function Hypotrochoid(outerRadius, innerRadius, distFromInnerCenter){
  * Creates a modified version of this curve so that it
  * fits the given radius.
  * @function
- * @param {number} Desired radius of the curve.
+ * @param {number} radius Desired radius of the curve.
  * @return {Hypotrochoid}
  */
  this.scaleTo=function(radius){
@@ -192,8 +253,7 @@ function Trochoid(radius, distFromCenter){
  /**
 * Generates a point on the curve from the given u coordinate.
 * @function
-* @param {number} v V coordinate.  This will sweep around the extruded
-* tube.
+* @param {number} u U coordinate.
 * @return {Array<number>} A 3-element array specifying a 3D point.
 * Only the X and Y coordinates will be other than 0.
 */
@@ -235,8 +295,7 @@ function Epitrochoid(outerRadius, innerRadius, distFromInnerCenter){
  /**
 * Generates a point on the curve from the given u coordinate.
 * @function
-* @param {number} v V coordinate.  This will sweep around the extruded
-* tube.
+* @param {number} u U coordinate.
 * @return {Array<number>} A 3-element array specifying a 3D point.
 * Only the X and Y coordinates will be other than 0.
 */
@@ -257,7 +316,7 @@ function Epitrochoid(outerRadius, innerRadius, distFromInnerCenter){
  * Creates a modified version of this curve so that it
  * fits the given radius.
  * @function
- * @param {number} Desired radius of the curve.
+ * @param {number} radius Desired radius of the curve.
  * @return {Epitrochoid}
  */
  this.scaleTo=function(radius){
