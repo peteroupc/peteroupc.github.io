a388de09cc5035bf9e48ef62bf4b751b10ed2ee5
Peter O <poccil14@gmail.com>
2016-11-25 08:47:31 -0500

    add build files

---
+++ build/makecss.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+Dir.chdir(File.dirname(__FILE__))
+`lessc style.less > style.css`
+plugins=%w( postcss-import cssnext postcss-colormin postcss-minify-font-values
+   autoprefixer postcss-merge-longhand postcss-minify-selectors
+   postcss-unique-selectors postcss-zindex
+   postcss-merge-rules postcss-merge-idents
+   postcss-reduce-idents postcss-discard-duplicates
+   postcss-discard-empty postcss-discard-comments
+   postcss-discard-unused cssnano )
+plugins.each{|f|
+ if !FileTest.directory?("/usr/local/lib/node_modules/#{f}")
+  puts f
+  `npm install -g #{f}`
+ end
+}
+cmd="postcss"
+plugins.each{|f|
+ cmd+=" --use #{f}"
+}
+cmd+=" --postcss-discard-comments.removeAll true -o ../style.css"
+puts cmd
+`#{cmd}`
