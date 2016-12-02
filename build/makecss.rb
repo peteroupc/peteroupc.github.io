05e44ca8d1ee0e85f37c308a7663a61412eef256
Peter O <poccil14@gmail.com>
2016-12-02 13:22:39 -0500

    don't use prefix in LESS source codes; let the postcss auto-prefix where needed

--- build/makecss.rb
+++ build/makecss.rb
@@ -1,13 +1,14 @@
 #!/usr/bin/ruby
 Dir.chdir(File.dirname(__FILE__))
-`lessc style.less > style.css`
-plugins=%w( postcss-import cssnext postcss-colormin postcss-minify-font-values
-   autoprefixer postcss-merge-longhand postcss-minify-selectors
+`lessc style.less > ../style.css`
+plugins=%w( postcss-import postcss-cssnext postcss-colormin postcss-minify-font-values
+   postcss-merge-longhand postcss-minify-selectors
    postcss-unique-selectors postcss-zindex
    postcss-merge-rules postcss-merge-idents
    postcss-reduce-idents postcss-discard-duplicates
    postcss-discard-empty postcss-discard-comments
    postcss-discard-unused cssnano )
+
 plugins.each{|f|
  if !FileTest.directory?("/usr/local/lib/node_modules/#{f}")
   puts f
@@ -18,6 +19,7 @@ cmd="postcss"
 plugins.each{|f|
  cmd+=" --use #{f}"
 }
-cmd+=" --postcss-discard-comments.removeAll true -o ../style.css"
+cmd+=" --postcss-discard-comments.removeAll true -o ../style.css < ../style.css"
 puts cmd
 `#{cmd}`
+
