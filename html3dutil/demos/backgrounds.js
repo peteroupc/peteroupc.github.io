4aa22f00b9bf292e87e128db7264281b913059b9
Peter O <poccil14@gmail.com>
2017-03-19 20:44:56 -0400

    Updating GitHub Pages

---
+++ html3dutil/demos/backgrounds.js
@@ -0,0 +1,84 @@
+/* global H3DU */
+/*
+ Any copyright to this file is released to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/
+ If you like this, you should donate
+ to Peter O. (original author of
+ the Public Domain HTML 3D Library) at:
+ http://peteroupc.github.io/
+*/
+/* exported StarburstBackground */
+function StarburstBackground(black, white) {
+  "use strict";
+  var vertex = "precision mediump float;attribute vec3 position;varying vec2 posVar;" +
+  "void main() {posVar=position.xy;gl_Position=vec4(position,1.0);}";
+  var fragment = [
+    "precision mediump float;",
+    "uniform vec3 blackColor;",
+    "uniform vec3 whiteColor;",
+    "varying vec2 posVar;",
+    "uniform float time;",
+    "void main() {",
+    " float angle=atan(posVar.y,posVar.x);",
+    " angle/=6.283185307;",
+    " angle=mod(angle+time*0.001,1.0); // Time range is [0,1000)",
+    " angle=floor(angle*24.0);",
+    " float s=(mod(angle,2.0)>=1.0) ? 1.0 : 0.0;",
+    " vec3 color=mix(blackColor,whiteColor,s);",
+    " gl_FragColor=vec4(color,1.0);",
+    "}"].join("\n");
+  this.shader = new H3DU.ShaderInfo(vertex, fragment);
+  this.shader.setUniforms({
+    "blackColor":H3DU.toGLColor(black).slice(0, 3),
+    "whiteColor":H3DU.toGLColor(white).slice(0, 3),
+    "time":0
+  });
+  this.batch = new H3DU.Batch3D().addShape(
+  new H3DU.Shape(new H3DU.Mesh(
+    [-1, 1, 0, -1, -1, 0,
+      1, 1, 0, 1, -1, 0],
+     [0, 1, 2, 2, 1, 3], 0)).setShader(this.shader));
+  this.getBatch = function() {
+    return this.batch;
+  };
+  this.update = function(time) {
+    this.shader.setUniforms({"time":time});
+  };
+}
+
+/* exported StripesBackground */
+function StripesBackground(black, white) {
+  "use strict";
+  var vertex = "precision mediump float;attribute vec3 position;varying vec2 posVar;" +
+  "void main() {posVar=position.xy;gl_Position=vec4(position,1.0);}";
+  var fragment = [
+    "precision mediump float;",
+    "uniform vec3 blackColor;",
+    "uniform vec3 whiteColor;",
+    "varying vec2 posVar;",
+    "uniform float time;",
+    "void main() {",
+    " float s;",
+    " s=mod((posVar.x+posVar.y+(time*0.001))*5.0,2.0);",
+    " s=(s>=1.0) ? 0.0 : 1.0;",
+    " vec3 color=mix(blackColor,whiteColor,s);",
+    " gl_FragColor=vec4(color,1.0);",
+    "}"].join("\n");
+  this.shader = new H3DU.ShaderInfo(vertex, fragment);
+  this.shader.setUniforms({
+    "blackColor":H3DU.toGLColor(black).slice(0, 3),
+    "whiteColor":H3DU.toGLColor(white).slice(0, 3),
+    "time":0
+  });
+  this.batch = new H3DU.Batch3D().addShape(
+  new H3DU.Shape(new H3DU.Mesh(
+    [-1, 1, 0, -1, -1, 0,
+      1, 1, 0, 1, -1, 0],
+     [0, 1, 2, 2, 1, 3], 0)).setShader(this.shader));
+  this.getBatch = function() {
+    return this.batch;
+  };
+  this.update = function(time) {
+    this.shader.setUniforms({"time":time});
+  };
+}
---
+++ html3dutil/demos/spiral.js
@@ -0,0 +1,48 @@
+/* global H3DU */
+/*
+ Any copyright to this file is released to the Public Domain.
+ http://creativecommons.org/publicdomain/zero/1.0/
+ If you like this, you should donate
+ to Peter O. (original author of
+ the Public Domain HTML 3D Library) at:
+ http://peteroupc.github.io/
+*/
+function spiralCurve(radius, phase) {
+  "use strict";
+  return new H3DU.Curve({
+/** @ignore */
+    "evaluate":function(u) {
+      var uphase = u + phase;
+      var cosu = Math.cos(uphase);
+      var sinu = uphase >= 0 && uphase < 6.283185307179586 ? uphase <= 3.141592653589793 ? Math.sqrt(1.0 - cosu * cosu) : -Math.sqrt(1.0 - cosu * cosu) : Math.sin(uphase);
+      var r = radius + u;
+      return [cosu * r, sinu * r];
+    },
+/** @ignore */
+    "endPoints":function() {
+      return [0, 6 * Math.PI];
+    }
+  });
+}
+
+function spiralBackgroundShape(color) {
+  "use strict";
+  var ce = new H3DU.CurveEval();
+  var mesh = new H3DU.Mesh();
+  for(var i = 0; i < 360; i += 10) {
+    var curve = spiralCurve(0.1, i * Math.PI * 2 / 360);
+    var length = curve.getLength();
+    var lines = Math.max(length / 0.2, 60);
+    ce.vertex(curve).evalCurve(mesh, H3DU.Mesh.LINES, lines);
+  }
+  return new H3DU.Shape(mesh).setMaterial(H3DU.Material.fromBasic(color));
+}
+
+/* exported spiralBatch */
+function spiralBatch() {
+  "use strict";
+  var batch = new H3DU.Batch3D();
+  batch.orthoAspect(-3, 3, -3, 3, -5, 5);
+  batch.addShape(spiralBackgroundShape("blue"));
+  return batch;
+}
