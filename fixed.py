ebf70d31671f8e2d58d5c58166ef36cd11e2ece0
Peter O <poccil14@gmail.com>
2019-09-08 18:58:21 -0400

    Updating GitHub Pages

--- fixed.py
+++ fixed.py
@@ -1,21 +1,26 @@
 from decimal import Decimal
-from math import floor
+import math
 
 class Fixed:
    """
    Fixed-point numbers, represented using integers that store multiples
-   of 1/FRAC.  They are not necessarily faster than floating-point numbers, nor
+   of 2^BITS.  They are not necessarily faster than floating-point numbers, nor
    do they necessarily have the same precision or resolution of floating-point
    numbers.  The main benefit of fixed-point numbers is that they improve
    determinism for applications that rely on non-integer real numbers (notably
    simulations and machine learning applications), whereas floating-point
-   numbers have a host of problems
-   that make repeatable results across computers difficult, including differences
-   in implementation, rounding behavior, and order of operations, as well as
-   nonassociativity of floating-point numbers.
+   numbers have a host of problems that make repeatable results across
+   computers difficult, including differences in implementation, rounding
+   behavior, and order of operations, as well as nonassociativity of
+   floating-point numbers.
+
+   Any copyright to this file is dedicated to the Public Domain, under
+   Creative Commons Zero version 1.0.
    """
-   # Fractional parts of a fixed-point number.
-   FRAC = 1000000
+   # Bits in the fraction of a fixed-point number
+   BITS = 20
+   MASK = (1<<BITS)-1
+   HALF = (1<<(BITS-1))
 
    def __init__(self, i):
      self.value=i
@@ -23,33 +28,91 @@ class Fixed:
    def v(i):
      if i.__class__==Fixed:
         return i
+     b = 0
+     neg = False
      if i.__class__==str:
-        return Fixed(int(Decimal(str)*Fixed.FRAC))
-     return Fixed(int(i*Fixed.FRAC))
+        d=Decimal(str)
+        neg=(d<0)
+        b = int(abs(Decimal(i)) * (1 << Fixed.BITS))
+     else:
+        neg=(i<0)
+        b = int(abs(i) * (1 << Fixed.BITS))
+     return Fixed(-b) if neg else Fixed(b)
 
    def __add__(a, b):
-     return Fixed(Fixed.v(a).value + Fixed.v(b).value)
+     av=Fixed.v(a).value
+     bv=Fixed.v(b).value
+     if (av>=0) == (bv>=0):
+        return Fixed(av + bv)
+     if a>=0: # a is nonnegative and b is negative
+        return Fixed(av - abs(bv))
+     else: # a is negative and b is nonnegative
+        return Fixed(bv - abs(av))
 
    def __sub__(a, b):
-     return Fixed(Fixed.v(a).value - Fixed.v(b).value)
+     return a + (-b)
 
    def __mul__(a, b):
-     return Fixed((Fixed.v(a).value * Fixed.v(b).value) // Fixed.FRAC)
+     av=Fixed.v(a).value
+     bv=Fixed.v(b).value
+     ava=abs(av)
+     bva=abs(bv)
+     ret=(ava*bva)
+     frac = ret&Fixed.MASK
+     ret = (ret>>Fixed.BITS)
+     # Rounding to nearest, ties to even
+     if (frac>Fixed.HALF or (frac==Fixed.HALF and (ret&1)==1)):
+        ret+=1
+     if (av>=0) != (bv>=0):
+        ret=-ret
+     return Fixed(ret)
+
+   def _div(a, b):
+     av=Fixed.v(a).value
+     bv=Fixed.v(b).value
+     ava=abs(av)
+     bva=abs(bv)
+     ret=ava<<Fixed.BITS
+     frac = ret % bva
+     ret = (ret // bva)
+     # Rounding to nearest, ties to even
+     if (bva&1)==0:
+       # Divisor's least significant bit is even
+       if (frac>(bva>>1) or (frac==(bva>>1) and (ret&1)==1)):
+         ret+=1
+     else:
+       if (frac>(bva>>1)):
+         ret+=1
+     if (av>=0) != (bv>=0):
+        ret=-ret
+     return Fixed(ret)
 
    def __rtruediv__(a, b):
-     return Fixed((Fixed.v(a).value * Fixed.FRAC) // Fixed.v(b).value)
+     return Fixed._div(a, b)
 
    def __rdiv__(a, b):
-     return Fixed((Fixed.v(a).value * Fixed.FRAC) // Fixed.v(b).value)
+     return Fixed._div(a, b)
 
    def __div__(a, b):
-     return Fixed((Fixed.v(a).value * Fixed.FRAC) // Fixed.v(b).value)
+     return Fixed._div(a, b)
 
    def __truediv__(a, b):
-     return Fixed((Fixed.v(a).value * Fixed.FRAC) // Fixed.v(b).value)
+     return Fixed._div(a, b)
 
    def __floordiv__(a, b):
-     return (a/b).floor()
+     av=Fixed.v(a).value
+     bv=Fixed.v(b).value
+     ava=abs(av)
+     bva=abs(bv)<<Fixed.BITS
+     ret=ava<<Fixed.BITS
+     oldret=ret
+     ret = (ret // bva)
+     if (av>=0) != (bv>=0):
+        frac = oldret % bva
+        if frac!=0:
+          ret+=1
+        ret=-ret
+     return Fixed.v(ret)
 
    def __mod__(a, b):
      return a - (a // b) * b
@@ -61,7 +124,7 @@ class Fixed:
    def __le__(self, other): return self.value <= Fixed.v(other).value
    def __eq__(self, other): return self.value == Fixed.v(other).value
    def __ne__(self, other): return self.value != Fixed.v(other).value
-   def __gt__(self, other): return self.value >  Fixed.v(other).value
+   def __gt__(self, other): return self.value > Fixed.v(other).value
    def __ge__(self, other): return self.value >= Fixed.v(other).value
    def __cmp__(self, other):
       othervalue = Fixed.v(other).value
@@ -70,7 +133,14 @@ class Fixed:
       return 1
 
    def floor(a):
-     return Fixed((Fixed.v(a).value // Fixed.FRAC) * Fixed.FRAC)
+     av=Fixed.v(a)
+     if av>=0:
+       return Fixed((av.value>>Fixed.BITS) << Fixed.BITS)
+     ava=abs(av.value)
+     if (ava&Fixed.MASK) != 0:
+       return Fixed(-( ((ava>>Fixed.BITS) + 1) << Fixed.BITS))
+     else:
+       return av
 
    def asin(a):
      return atan2(a, sqrt(Fixed.v(1)-a*a))
@@ -79,49 +149,62 @@ class Fixed:
      return atan2(sqrt(Fixed.v(1)-a*a), a)
 
    def round(a):
-     raise NotImplementedError()
+     av=Fixed.v(a).value
+     ava=abs(av)
+     ret=ava
+     frac = ret&Fixed.MASK
+     ret = (ret>>Fixed.BITS)
+     # Rounding to nearest, ties away from zero
+     if (frac>=Fixed.HALF):
+        ret+=1
+     if av<0:
+        ret=-ret
+     return Fixed.v(ret)
 
    def sin(a):
-     raise NotImplementedError()
+     raise NotImplementedError
 
    def cos(a):
-     raise NotImplementedError()
+     raise NotImplementedError
 
    def tan(a):
-     raise NotImplementedError()
+     raise NotImplementedError
 
    def atan2(y, x):
-     raise NotImplementedError()
+     x=Fixed.v(x)
+     if y==0: return 0
+     raise NotImplementedError
 
    def pow(a, b):
-     raise NotImplementedError()
+     raise NotImplementedError
 
-   def sqrt(a, b):
-     raise NotImplementedError()
+   def sqrt(a):
+     if a<0: raise ValueError
+     if a==0: return Fixed(0)
+     raise NotImplementedError
 
    def log(a):
-     raise NotImplementedError()
+     raise NotImplementedError
 
    def exp(a):
-     raise NotImplementedError()
+     raise NotImplementedError
 
    def __str__(self):
-     return str(Decimal(self.value)/Decimal(Fixed.FRAC))
+     return str(Decimal(self.value)/Decimal((1<<Fixed.BITS)))
+
+   def __repr__(self):
+     return str(Decimal(self.value)/Decimal((1<<Fixed.BITS)))
 
 if __name__ == "__main__":
-   print(Fixed.v(10))
-   print(Fixed.v(10)+Fixed.v(5))
-   print(Fixed.v(10)*Fixed.v(5))
-   print(Fixed.v(10)/Fixed.v(5))
-   print(Fixed.v(10)/Fixed.v(3))
-   print(Fixed.v(10)//Fixed.v(3))
-   print(Fixed.v(10)%Fixed.v(3))
-   print(Fixed.v(10)/5)
-   print(Fixed.v(10)/3)
-   print(Fixed.v(-1))
-   print(Fixed.v(3))
-   print(Fixed.v(3.5))
-   print(Fixed.v(3.5).floor())
-   print(Fixed.v(-3.5).floor())
-   print(min(Fixed.v(10),Fixed.v(5)))
-   print(max(Fixed.v(10),Fixed.v(5)))
+   def asserteq(a,b):
+      if a!=b: raise ValueError(str([a, b]))
+   asserteq(Fixed.v(-0.75), Fixed.v(-3)/4)
+   asserteq(Fixed.v(-1), Fixed.v(-3)//4)
+   asserteq(Fixed.v(15.75), Fixed.v(3.5)*4.5)
+   asserteq(Fixed.v(1), Fixed.v(-3)%4)
+   asserteq(Fixed.v(0), Fixed.v(0.5).floor())
+   asserteq(Fixed.v(1), Fixed.v(0.5).round())
+   asserteq(Fixed.v(0), Fixed.v(0.1).round())
+   asserteq(Fixed.v(1), Fixed.v(0.9).round())
+   asserteq(Fixed.v(-1), Fixed.v(-0.5).floor())
+   asserteq(Fixed.v(-1), Fixed.v(-1).floor())
